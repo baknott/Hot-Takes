8c1823536fa45ad0041fca52d922a9dfe9119abe
baknott <poujolbryan@gmail.com>
2023-01-03 00:57:22 +0100

    Sécurité signup/login sur l'email de l'utilisateur

--- controllers/user.js
+++ controllers/user.js
@@ -6,10 +6,12 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 const AUTH_TOKEN = process.env.SECRET_TOKEN;
-
+const patternEmail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
 //Inscription sur Hot Takes
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
+    //Vérification de la syntaxe de l'email coté backend
+    if(patternEmail.test(req.body.email)){
+        bcrypt.hash(req.body.password, 10)
       .then(hash => {
         const user = new User({
           email: req.body.email,
@@ -20,11 +22,16 @@ exports.signup = (req, res, next) => {
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
+    }else{
+        console.log("Erreur dans la syntaxe de l'email")
+    };
+    
   };
 
   //Connexion a son compte Hot Takes
   exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    if(patternEmail.test(req.body.email)){
+        User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur et/ou mot de passe incorrect' });
@@ -46,4 +53,8 @@ exports.signup = (req, res, next) => {
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
+    }else{
+        console.log("Erreur dans la syntaxe de l'email")
+    };
+    
  };
